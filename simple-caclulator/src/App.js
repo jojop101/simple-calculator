afb140908b493e0c8d49766873504ecb94d766bb
Giuseppe-Pennisi <giuseppe.d.pennisi@gmail.com>
2022-03-11 15:24:43 -0500

    Buttons register digits and operations

--- simple-caclulator/src/App.js
+++ simple-caclulator/src/App.js
@@ -1,31 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
 import './styles.css'
+import { useReducer } from "react"
+import DigitButtons from './DigitButtons';
+import OperationButtons from './OperationButtons'
+
+
+export const ACTIONS = {
+  ADD_DIGIT: "add_digit",
+  CHOOSE_OPERATION: "choose_operation",
+  CLEAR: 'clear',
+  DELETE_DIGIT: "delete_digit",
+  EVALUATE: "evaluate",
+
+}
+function reducer(state, {type, payload}) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      if (payload.digit === "0" && state.currentOperand === "0") return state
+      if (payload.digit === "." && state.currentOperand.includes(".")) return state
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      }
+    
+    case ACTIONS.CHOOSE_OPERATION: 
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state
+      }
+    
+      if (state.previousOperand == null) {
+        return {
+          ...state, 
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null
+        }
+      }
+    case ACTIONS.CLEAR: {
+      return {}
+    }
+  }
+
+}
 
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
+  
+  
+
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
-      <button className="span-two">AC</button>
+      <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR})} >AC</button>
       <button>Del</button>
-      <button>÷</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
+      <OperationButtons operation="÷" dispatch={dispatch} />
+      <DigitButtons digit="1" dispatch={dispatch} />
+      <DigitButtons digit="2" dispatch={dispatch} />
+      <DigitButtons digit="3" dispatch={dispatch} />
+      <OperationButtons operation="*" dispatch={dispatch} />
+      <DigitButtons digit="4" dispatch={dispatch} />
+      <DigitButtons digit="5" dispatch={dispatch} />
+      <DigitButtons digit="6" dispatch={dispatch} />
+      <OperationButtons operation="+" dispatch={dispatch} />
+      <DigitButtons digit="7" dispatch={dispatch} />
+      <DigitButtons digit="8" dispatch={dispatch} />
+      <DigitButtons digit="9" dispatch={dispatch} />
+      <OperationButtons operation="-" dispatch={dispatch} />
+      <DigitButtons digit="." dispatch={dispatch} />
+      <DigitButtons digit="0" dispatch={dispatch} />
       <button className="span-two">=</button>
     </div>
   );
